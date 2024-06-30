002cc8af04d89d17a25ed48696b2c4c9c1848e57
Abdul Nayeemuddin Rashed <159563338+ECE-MJCET@users.noreply.github.com>
2024-06-30 22:47:57 +0530

    Add files via upload

---
+++ c++ programming/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "hello, world!" << endl;
+    std::cout << "hello, welcome to c++ programing" << std::endl;
+    cout << "hello, c++ programming";
+    return 0;
+}
+
